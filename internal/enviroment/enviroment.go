1a0ae5255ccf26c0da9ee1a4a1f2df74389f3c83
Fran <traversafranco9@gmail.com>
2026-02-21 17:45:46 -0300

    .env universal

--- internal/enviroment/enviroment.go
+++ internal/enviroment/enviroment.go
@@ -3,5 +3,9 @@ package enviroment
 import "github.com/joho/godotenv"
 
 func LoadEnviroment(key_env string) {
-	_ = godotenv.Load(".env." + key_env)
+	if key_env != "" {
+		_ = godotenv.Load(".env." + key_env)
+	} else {
+		_ = godotenv.Load(".env")
+	}
 }
