a58217cb6855134621f095029f1142ef732a4b6c
Fran <traversafranco9@gmail.com>
2026-05-13 00:28:20 -0300

    done

--- internal/services/domain/auth_domain.go
+++ internal/services/domain/auth_domain.go
@@ -2,13 +2,14 @@ package domain
 
 import "github.com/francotraversa/Sliceflow/internal/types"
 
-type AuthRepository interface {
+// AuthUseCase define la lógica de negocio de autenticación
+type AuthUseCase interface {
 	Login(userCreds types.UserLoginCreds) (*types.TokenResponse, error)
+}
+
+// AuthRepository define las operaciones de base de datos para auth
+type AuthRepository interface {
 	CheckUser(userCreds types.UserLoginCreds) (bool, error)
 	CheckPassword(userCreds types.UserLoginCreds) (bool, error)
 	GetUser(username string) (*types.User, error)
-}
-
-type AuthUseCase interface {
-	Login(userCreds types.UserLoginCreds) (*types.TokenResponse, error)
 }
