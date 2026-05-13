5086937090f2437d09783833c069c43ff46fe36c
Fran <traversafranco9@gmail.com>
2026-05-13 00:40:26 -0300

    fix

---
+++ internal/middlewares/role.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/francotraversa/Sliceflow/internal/types"
+	"github.com/labstack/echo/v4"
+)
+
+func RequireRole(role string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			claims, err := GetClaimsFromContext(c)
+			if err != nil {
+				return c.JSON(http.StatusInternalServerError, types.Error{Error: "failed to parse custom claims"})
+			}
+
+			if claims.Role != role && claims.Role != "owner" {
+				return c.JSON(http.StatusForbidden, types.Error{Error: "permission denied"})
+			}
+
+			return next(c)
+		}
+	}
+}
